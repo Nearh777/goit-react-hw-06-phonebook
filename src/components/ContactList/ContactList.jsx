07929303b35c28f9352b64799dd7bb8918cd2146
Nearh777 <nearh777@gmail.com>
2023-06-27 10:27:34 +0300

    updete

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,45 @@
+import { Wrapper, Item } from './ContactList.styled';
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useDispatch, useSelector } from 'react-redux';
+import { IoMdPerson } from 'react-icons/io';
+import { deleteContact } from 'redux/contactsSlice';
+import { getContacts, getFilter } from 'redux/selectors';
+
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const contactsFilter = useSelector(getFilter);
+  const handleDeleteButton = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(contactsFilter.toLowerCase())
+  );
+  return (
+    <>
+      <Wrapper>
+        {visibleContacts.map(contact => (
+          <Item key={contact.id}>
+            <p>
+              <span>
+                <IoMdPerson />
+              </span>
+              {contact.name}: {contact.number}
+            </p>
+
+            <Button
+              type="button"
+              onClick={() => handleDeleteButton(contact.id)}
+              variant="outlined"
+              startIcon={<DeleteIcon />}
+              size="small"
+            >
+              Delete
+            </Button>
+          </Item>
+        ))}
+      </Wrapper>
+    </>
+  );
+};
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,8 @@
-import { createGlobalStyle } from 'styled-components';
+// import { createGlobalStyle} from '@emotion/styled';
+import styled from '@emotion/styled'
 
-export const GlobalStyle = createGlobalStyle`
+
+ export const GlobalStyle = styled.createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
@@ -30,3 +32,4 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
 `;
+
